43fbcac1e312207ba3a13520bdacef4080bc09bb
Tom <tomasvergara272@gmail.com>
2023-04-27 01:23:45 -0300

    counter encapsulated

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,10 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector:'app-counter',
     templateUrl:'./counter.component.html',
     styleUrls:['./counter.component.scss']
 })
-export class CounterComponent{
+export class CounterComponent implements OnInit{
+
+    public counter: number = 0;
+
+    ngOnInit(): void {}
+
+    decreaseBy(value:number) :void {
+        this.counter -= value;
+    }
+    increaseBy(value:number) :void {
+        this.counter += value;
+    }
+    resetCounter():number{
+        return this.counter = 0;
+    }
 
 }
