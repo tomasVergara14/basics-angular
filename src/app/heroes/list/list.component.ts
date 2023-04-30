f4cfecb3d20fa5851e3a8acdbb08cc7ad1d83c12
Tom <tomasvergara272@gmail.com>
2023-04-30 17:49:08 -0300

    ng template

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
+  
+  public heroNames: string[] = ['spiderman', 'ironman', 'hulk', 'thor', 'captain america'];
+  public deletedHero?: string = ''
+
+
+  onRemoveLastHero() :void {
+    this.deletedHero = this.heroNames.pop();
+  }
 
 }
